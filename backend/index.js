cd6cbe5bcb4e0fd1420e815fcd2bec670c1eee41
Vikas Verma <vikasverma3552@gmail.com>
2025-06-29 19:49:40 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOption={
-    origin:"http://localhost:5173",
+    origin:process.env.FORNTEND_URI,
     credentials: true,
 }
 app.use(cors(corsOption))
